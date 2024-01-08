1b38c269dee8e5b78e07c3195c15c086ce5f61b6
Atri <mahapatraatri@gmail.com>
2024-01-08 16:01:30 +0530

    Data Ingestion done

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,6 @@
+##Those things which are constant throughout would be kept in these constant folder
+
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
